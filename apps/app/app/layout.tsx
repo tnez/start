bb4a1f3c110315cbca44f197246179203c67ab05
Travis Nesland <tnesland@gmail.com>
2022-10-31 15:53:41 +0000

    Small formatting fixes

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
-import {
-	ClerkProvider,
-	SignedIn,
-	UserButton,
-} from '@clerk/nextjs/app-beta'
+import { ClerkProvider, SignedIn, UserButton } from '@clerk/nextjs/app-beta'
 import './globals.css'
 
 export default function RootLayout({
