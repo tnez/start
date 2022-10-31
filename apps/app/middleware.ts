21bc0805e6a43915f804a86861421f8c1ed5e5da
Travis Nesland <tnesland@gmail.com>
2022-10-31 14:02:36 +0000

    Configure additional UI for auth

--- apps/app/middleware.ts
+++ apps/app/middleware.ts
@@ -1,7 +1,33 @@
-import { withClerkMiddleware } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
+import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export default withClerkMiddleware((/* request: NextRequest */) => {
+const PUBLIC_PATHS = ['/sign-in*'] as const
+
+function doesMatchPublicPath(path: string) {
+	return PUBLIC_PATHS.find((x) =>
+		path.match(new RegExp(`^${x}$`.replace('*$', '($|/)'))),
+	)
+}
+
+function generateSignInUrl(redirectPath: string) {
+	const url = new URL('/sign-in', redirectPath)
+	url.searchParams.set('redirect_url', redirectPath)
+
+	return url
+}
+
+export default withClerkMiddleware((request: NextRequest) => {
+	if (doesMatchPublicPath(request.nextUrl.pathname)) {
+		return NextResponse.next()
+	}
+
+	const { userId } = getAuth(request)
+
+	if (userId === null) {
+		const signInUrl = generateSignInUrl(request.url)
+		return NextResponse.redirect(signInUrl)
+	}
+
 	return NextResponse.next()
 })
 
